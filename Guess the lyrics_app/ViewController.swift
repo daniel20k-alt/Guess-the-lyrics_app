60c5081c84723f594f532cd542d30aaadc08736d
Testing_author <testing_beta@mail.com>
2020-02-13 08:24:56 +0200

    Added the "scoreLabel"

--- Guess the lyrics_app/ViewController.swift
+++ Guess the lyrics_app/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var cluesLabel: UILabel!
+    var answersLabel: UILabel!
+    var currentAnswer: UILabel!
+    var scoreLabel: UILabel!
+    var letterButtons = [UIButton]()
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        // additional settings should be added
+        scoreLabel = UILabel()
+        scoreLabel.translatesAutoresizingMaskIntoConstraints = false
+        scoreLabel.textAlignment = .right
+        scoreLabel.text = "Score: 0"
+        view.addSubview(scoreLabel)
+    }
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
